c7159cdcaaac842a20c8251afa3969c8e8cdb992
GabrielCoutinho <eng.gabrielcoutinho@outlook.com.br>
2024-03-11 16:23:49 -0300

    correction to deploy

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function SectionTitle({ children }: String) {
+function SectionTitle({ children }: any) {
   return (
     <h2 className='text-sky-950 text-3xl m-5 mb-20'>{children}</h2>
   )
