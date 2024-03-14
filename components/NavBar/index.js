adbc6bafcf698c3b42fb4ea1056a7ce23fa0e561
GabrielCoutinho <eng.gabrielcoutinho@outlook.com.br>
2024-03-13 23:24:21 -0300

    width adjust of NavBar for mobile layout

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -19,7 +19,7 @@ export default function NavBar({ children }) {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="flex mt-2 fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 bg-neutral-50 rounded-full">
+      <nav className="flex mt-2 fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 bg-neutral-50 rounded-full lg:w-2/5 w-full justify-center">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
 
           {children.map(item => (
